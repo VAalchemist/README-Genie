e17bd6a48505abcd6c48cfbba8e0dccf7e3207d4
VAalchemist <92349345+VAalchemist@users.noreply.github.com>
2022-02-20 22:27:49 -0800

    exports, imports, const, err

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,29 @@
+const fs = require('fs');
+const markdownDataArgs = process.argv.slice(2);
+const [data] = markdownDataArgs;
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+renderLicenseLink(license, err => {
+    if (err) throw new Error(err);
+
+    console.log('Markdown is ready! Feast your eyes on your new README.md!');
+ });
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
+fs.writeFile('README.md', generateMarkdown(data)=> {
   return `# ${data.title}
 
 `;
-}
+});
 
 module.exports = generateMarkdown;
